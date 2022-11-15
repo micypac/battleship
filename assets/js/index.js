8269486f8ec2823ac25526dfe8fb811b977121df
Michael Pacleb <michael.pacleb@gmail.com>
2022-11-15 16:48:34 -0500

    Implemented reset game button and its event listener.

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,12 +1,31 @@
 import Board from "./board.js";
 
-let board = new Board(); // creates a new game board
+let board;
 
-// Examine the grid of the game board in the browser console.
-// Create the UI of the game using HTML elements based on this grid.
-console.log(board.grid);
+// Game initialization.
+const initializeGame = () => {
+  board = new Board(); // creates a new board game
+  console.log(board.grid);
 
-// square event listener callback function
+  const mainSection = document.createElement("section");
+  mainSection.classList.add("main-section");
+  mainSection.setAttribute("id", "main");
+  document.body.appendChild(mainSection);
+
+  for (let i = 0; i < board.grid.length; i++) {
+    for (let j = 0; j < board.grid[0].length; j++) {
+      const square = document.createElement("div");
+      square.classList.add("box");
+      square.setAttribute("data-x", i);
+      square.setAttribute("data-y", j);
+      mainSection.appendChild(square);
+
+      square.addEventListener("click", checkHit);
+    }
+  }
+};
+
+// Board squares event listener callback function
 const checkHit = (event) => {
   const square = event.target;
   // call board method with square coordinates as argument and check if hit any ships
@@ -15,7 +34,7 @@ const checkHit = (event) => {
   if (hitResult !== null) {
     event.target.innerText = hitResult;
     event.target.classList.add("box-hit");
-    console.log(board.numRemaining);
+    // console.log(board.numRemaining);
 
     isGameOver();
   } else {
@@ -23,33 +42,46 @@ const checkHit = (event) => {
   }
 };
 
+// Helper function to determine if game over.
 const isGameOver = () => {
   if (board.isGameOver()) {
+    // grab all grid cells and remove their event listeners when game is over
     const gridSquares = document.querySelectorAll(".box");
     gridSquares.forEach((square) => {
       square.removeEventListener("click", checkHit);
     });
 
-    h2Element.style.visibility = "visible";
+    gameOverMessage();
   }
 };
 
-const h2Element = document.createElement("h2");
-h2Element.innerText = "YOU WIN!!!";
-h2Element.style.visibility = "hidden";
-document.body.appendChild(h2Element);
-const mainSection = document.createElement("section");
-mainSection.classList.add("main-section");
-document.body.appendChild(mainSection);
+// Display game over message.
+const gameOverMessage = () => {
+  // create new h2 element with the message
+  const h2Element = document.createElement("h2");
+  h2Element.innerText = "YOU WIN!!!";
 
-for (let i = 0; i < board.grid.length; i++) {
-  for (let j = 0; j < board.grid[0].length; j++) {
-    const square = document.createElement("div");
-    square.classList.add("box");
-    square.setAttribute("data-x", i);
-    square.setAttribute("data-y", j);
-    mainSection.appendChild(square);
+  // remove first the reset button from the body before appending h2 element
+  divElement.removeChild(resetButton);
+  divElement.appendChild(h2Element);
+};
 
-    square.addEventListener("click", checkHit);
-  }
-}
+// Reset button even handler
+const resetGame = () => {
+  // grab the current "main" section element from the document and remove it before initializing the game.
+  const mainSection = document.getElementById("main");
+  document.body.removeChild(mainSection);
+
+  initializeGame();
+};
+
+const divElement = document.createElement("div");
+document.body.appendChild(divElement);
+const resetButton = document.createElement("button");
+resetButton.setAttribute("id", "reset-button");
+resetButton.textContent = "Reset Game";
+divElement.appendChild(resetButton);
+
+resetButton.addEventListener("click", resetGame);
+
+initializeGame();
